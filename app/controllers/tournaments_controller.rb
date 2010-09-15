86f3cdcaa9f477f4708ae007087eacc38d34a6f8
Seth Ladd <sethladd@gmail.com>
2010-09-15 11:46:10 -0700

    show tournament, create tournament

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,4 +2,15 @@ class TournamentsController < ApplicationController
   def new
     @tournament = Tournament.new
   end
+  def create
+    @tournament = Tournament.new(params[:tournament])
+    if @tournament.save
+      redirect_to @tournament
+    else
+      render :action => :new
+    end
+  end
+  def show
+    @tournament = Tournament.find(params[:id])
+  end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,5 @@
 class Tournament < ActiveRecord::Base
+  validates :name, :presence => true
   scope :in_progress, lambda { where('started_at < ?', Time.now) }
   scope :upcoming, lambda { where('started_at > ?', Time.now) }
 end
