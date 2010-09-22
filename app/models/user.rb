d862f6aa6540e9f7f500c0bd6b3c450c42ef8b60
Seth Ladd <sethladd@gmail.com>
2010-09-21 23:18:48 -0700

    make user first and last name required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :participations, :class_name => 'Participant', :dependent => :destroy
   
-  validates :identifier_url, :email, :presence => true
+  validates :identifier_url, :email, :first_name, :last_name, :presence => true
 end
