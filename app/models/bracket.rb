f11f9bcc49a17a9b0ddafa17e0469523d08c968d
Seth Ladd <sethladd@gmail.com>
2010-10-01 17:14:42 -0400

    remove unused method

--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,8 +1,4 @@
 class Bracket < ActiveRecord::Base
   belongs_to :tournament
   has_many :matches, :dependent => :destroy
-  
-  def first_round_matches
-    matches.select{|m| m.round == 0}
-  end
 end
