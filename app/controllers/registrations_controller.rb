6a0e76ce1007d3f3fd95ac933407210fa98e8efc
Seth Ladd <sethladd@gmail.com>
2010-10-09 01:20:32 -0400

    fix misspelling

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
   
   def create
     @tournament = Tournament.find(params[:tournament_id])
-    @participant = @tournament.registrations.build(:user => current_user)
+    @registration = @tournament.registrations.build(:user => current_user)
     unless @registration.save
       flash[:error] = "Uh oh: #{@registration.errors.full_messages.join("<br>")}"
     end
