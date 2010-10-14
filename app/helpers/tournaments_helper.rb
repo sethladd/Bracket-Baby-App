ce91c8c2d119518189316491d2f9abb5c82c5bc4
Jan Kleinert <jkleinert@jkleinert-macbookpro.local>
2010-10-13 20:49:33 -0400

    updates to bracket rendering to handle all size brackets up to/including 16.

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -3,26 +3,22 @@ module TournamentsHelper
   def display_svg_bracket(bracket)
     not_visited_matches = bracket.matches.dup
     output = ''
-    is_first_match = true
-    first_match_id = ''
+    match_iter = 0
     
     bracket.matches.select{|m| m.round == 0}.each do |first_round_match|
-      if (is_first_match)
-        first_match_id = first_round_match.id
-        is_first_match = false
-      end
-      output << display_svg_match(first_round_match, not_visited_matches, 0, first_match_id)
+      output << display_svg_match(first_round_match, not_visited_matches, 0,  match_iter)
+      match_iter = match_iter+1
     end
     
     output.html_safe
   end
   
-  def display_svg_match(match, not_visited_matches, round, starting_match_id)
+  def display_svg_match(match, not_visited_matches, round, match_iter)
     return '' unless match
     
-    output = render('bracket_box', :match => match, :round => round, :players => match.match_players.sort_by{|mp| mp.id}, :starting_match => starting_match_id)
+    output = render('bracket_box', :match => match, :round => round, :players => match.match_players.sort_by{|mp| mp.id}, :match_iter => match_iter)
     not_visited_matches.delete(match)
-    output << display_svg_match(not_visited_matches.detect{|m| m.preceded_by?(match)}, not_visited_matches, round+1, starting_match_id)
+    output << display_svg_match(not_visited_matches.detect{|m| m.preceded_by?(match)}, not_visited_matches, round+1, match_iter)
     output.html_safe
   end  
   
