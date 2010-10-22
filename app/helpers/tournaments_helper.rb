b15d8e10a821be010eb4a81aede0de6c49f71c64
Jan Kleinert <jkleinert@jkleinert-macbookpro.local>
2010-10-21 22:02:11 -0400

    display match players in subsequent rounds in the correct order in the SVG bracket

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -5,16 +5,21 @@ module TournamentsHelper
     output = ''
     
     bracket.matches.select{|m| m.round == 0}.each_with_index do |first_round_match, match_iter|
-      output << display_svg_match(first_round_match, not_visited_matches, 0, match_iter)
+      output << display_svg_match(first_round_match, not_visited_matches, 0, match_iter, first_round_match)
     end
     
     output.html_safe
   end
   
-  def display_svg_match(match, not_visited_matches, round, match_iter)
+  def display_svg_match(match, not_visited_matches, round, match_iter, preceding_match)
     return '' unless match
     
     players = match.match_players.sort_by{|mp| mp.id}
+    if (preceding_match != match && !preceding_match.winner.nil? &&
+        (preceding_match.match_players.first.nickname != players.first.nickname && 
+         preceding_match.match_players.last.nickname != players.first.nickname))
+      players = match.match_players.sort_by{|mp| mp.id}.reverse
+    end
     
     output = render('bracket_box_svg',
                       :match => match,
@@ -23,7 +28,7 @@ module TournamentsHelper
                       :match_iter => match_iter)
     not_visited_matches.delete(match)
     next_match = not_visited_matches.detect{|m| m.preceded_by?(match)}
-    output << display_svg_match(next_match, not_visited_matches, round+1, match_iter)
+    output << display_svg_match(next_match, not_visited_matches, round+1, match_iter, match)
     output.html_safe
   end
   
