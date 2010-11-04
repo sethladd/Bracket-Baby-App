c4661cca192bc2d4841f5cd94375dce825259e53
Jan Kleinert <jkleinert@dhcp-172-29-92-76.cnc.corp.google.com>
2010-11-04 16:41:17 -0400

    Bracket Baby UI upgrade

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+  def index
+    @upcoming_tournaments = Tournament.upcoming
+    @in_progress_tournaments = Tournament.started_or_should_start
+    @finished_tournaments = Tournament.ended
+  end  
 end
